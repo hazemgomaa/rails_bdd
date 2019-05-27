bc09702275e3382fd677928108a5abeaaf4e6e39
Hazem Gomaa <gomaa.hazem@gmail.com>
2019-05-26 18:51:38 -0600

    install and run cucumber

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.8'
   gem 'factory_bot_rails'
+  
+  gem 'cucumber-rails', require: false
+  gem 'database_cleaner'
 end
 
 group :development do
--- spec/models/worker_spec.rb
+++ spec/models/worker_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Worker, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
   it do
-    expect(1 + 200).to eq(0)
+    expect(1).to eq(1)
   end
 end
